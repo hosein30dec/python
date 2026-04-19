59740df6ac6dad468a27b89fe9b5a41d3d3c4378
hosein30dec <hosein30dec@gmail.com>
2026-04-19 21:39:04 +0330

    ramz_obor_banki

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-import sys  
-
-print("Hello World ! I'm Her e ")
-print("Hello World ! I'm Here ")
+correct_password="admin"
+Tedad_ramz=0
+while Tedad_ramz<3 :
+    ramz=input("Ramz vorod khoda ra vared konid :")
+    if ramz==correct_password :
+        print("Ramz Sahih ast khosh omadid")
+        break
+    else :
+        Tedad_ramz=Tedad_ramz+1
+        forsat_ramz=3-Tedad_ramz
+        print(f"forsat vared kardan ramz shoma {forsat_ramz} ast pas ba deqat vared konid")
+    if Tedad_ramz==3 :
+        print("hesab shoma masdod shod.")
